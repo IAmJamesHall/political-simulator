225fdcbe4c8ac323385307a6a05f2c53715be1b4
James Hall <hi@jameshall.xyz>
2023-05-02 22:04:31 +0000

    Basic door-knocking

--- myapp/src/routes/+page.svelte
+++ myapp/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+let vars = {
+    clout: 0,
+    money: 0,
+    people: 0,
+    doors: 0,
+    peopleCost: 10,
+}
+
+const updateVars = () =>{
+    vars.doors += vars.people;
+}
+
+// run updateVars every second
+setInterval(updateVars, 1000);
+
+const hireSomeone = () => {
+    if (vars.money >= vars.peopleCost) {
+        vars.people += 1;
+        vars.money -= vars.peopleCost;
+    } else {
+        alert("You don't have enough money to hire someone.");
+    }
+}
+</script>
+
+
+<h1>Political Simulator</h1>
+<p>Talk to people in your town to gain clout.</p>
+//button to talk to people
+<button on:click={() => vars.doors += 1}>Knock on doors</button>
+
+<p>Hire someone to knock on doors for you.</p>
+<button on:click={hireSomeone}>Hire someone</button>
+
+<p>Doors: {vars.doors}</p>
